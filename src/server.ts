3d47e50c0dbdd065af185507ab702c0953dc5161
Md. Arman Hossain <mahpro110@gmail.com>
2024-12-16 07:55:16 +0600

    backend ready to implement docker activities

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { Server } from "http";
+import app from "./app";
+import { errorLogger, logger } from "./app/src/shared/logger";
+
+let server: Server;
+
+const PORT = 5000;
+
+async function main() {
+  try {
+    server = app.listen(PORT, () => {
+      console.log(`app is listening on port ${PORT}`);
+      logger.info(`app is listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+    errorLogger.error(err);
+  }
+}
+
+main();
+
+process.on("unhandledRejection", err => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
+  errorLogger.error(err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  errorLogger.error("uncaughtException is detected");
+  process.exit(1);
+});
